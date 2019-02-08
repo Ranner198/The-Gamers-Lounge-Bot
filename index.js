dbb68d54ed43ea616856d45ec9a428d77afb1531
Ranner198 <rancrump18@gmail.com>
2019-02-08 11:30:57 -0600

    Day 2

--- index.js
+++ index.js
@@ -2,25 +2,39 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
 
+
+//rainbow text module
+const chalkAnimation = require('chalk-animation');
+console.log('Connecting...')
+
 // create a new Discord client
 const client = new Discord.Client();
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
-client.once('ready', () => {
+client.once('ready', () => {	
+	//Clear the console
+	console.log('\033[2J');
+	console.log('\033[H');
+
+	//On connection display this bot's name
 	console.log(`Logged in as ${client.user.tag}!`);
-	console.log('Ready!');
+
+	//Start a connected symbol
+	const rainbow = chalkAnimation.rainbow('Connected' + 
+		'\n\nWelcome to The-Gamers-Lounge Bot created by: Ran Crump'); // Animation starts
+	setTimeout(() => {
+	    rainbow.stop(); // Stop animation after 8 seconds
+	}, 8000);
 });
 
 // login to Discord with your app's token
 client.login(auth.token);
 
-
-
-//Messaging System
+//On Message Recieved
 client.on('message', message => {
 
-	
+	//If the message is from the Bot ignore it
 	if (message.author == client.user)
 		return;
 	
@@ -31,23 +45,52 @@ client.on('message', message => {
         var cmd = args[0];
 		switch(cmd) {
 		case 'lfg':			
-			//message.reply(' is currently in room: ' + message.member.voiceChannel);			
-			//message.channel.send('Invite:' + Invite.channel);
-			var options = {
-				maxAge: 0
-			};
-			if(message.member.voiceChannel != null)
-			{
-				var invite = message.member.voiceChannel.createInvite(options).then((newInvite) => {
-					message.channel.send("https://discord.gg/" + newInvite.code).then(msg => {
-						msg.delete(180000)
-					})
-				})
-				.catch((e)=>{message.channel.send('Error generating invite: (' + e + ')') });
 
-				message.delete(1000); 		
+				let options = {
+					maxAge: 0
+				};
+
+				let reference = message.member.voiceChannel;
+				let outputReference;
+			if (args.length > 3)
+			{
+				let tempReturnMessage = message.content.substring(4, message.content.length);				
+
+				if(message.member.voiceChannel != null)
+				{
+					outputReference = reference.createInvite(options).then((newInvite) => {
+						message.channel.send('https://discord.gg/' + newInvite.code + ' ' + tempReturnMessage).then(msg => { //Embed user Icon and find a way to make hyperlinks :)
+							msg.delete(180000);			
+						})
+						.catch((e)=>{console.log('Error generating invite: (' + e + ')') });
+					})
+					.catch((e)=>{console.log('Error generating invite: (' + e + ')') });
+					message.delete(1000); 	
+					return;	
+				}				
+			} else {
+				if(message.member.voiceChannel != null)
+				{
+					outputReference = reference.createInvite(options).then((newInvite) => {
+						message.channel.send("https://discord.gg/" + newInvite.code).then(msg => {
+							msg.delete(180000);	
+						})
+						.catch((e)=>{console.log('Error generating invite: (' + e + ')') });
+					})
+					.catch((e)=>{console.log('Error generating invite: (' + e + ')') });
+					message.delete(1000); 	
+					return;		
+				}				
 			}
 		break;
 		}
 	}
 });
+
+
+
+
+
+
+
+
